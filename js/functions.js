e1d805e5e387217540d0b18bfbb5c9df89332913
Kell0301 <alikellar@gmail.com>
2012-01-13 11:36:21 -0500

    added prompt, console and document

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,17 @@
 // JavaScript Document
 /*
 Javascript multi-line comment 
-*/*
+*/
+
+//'alert' pops up a little window with an ok button
+alert('This is an alert');
+
+//'prompt()' is for user input, has a text field for the user to enter some text
+var name = prompt('What is your name?');
+
+//'console.log()' writes out the content of a variable for debugging
+//it is identical to Actionscript's 'trace'
+console.log(name);
+
+document.write('Hello, ' + name);
+
